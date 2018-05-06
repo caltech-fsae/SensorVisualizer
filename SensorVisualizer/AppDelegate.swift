5738534b0c9d3e89f9798aebe90dde28ac53d59d
Vignesh Varadarajan <vigneshv@gmail.com>
2018-05-06 14:15:53 -0700

    Initial Commit

---
+++ SensorVisualizer/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  SensorVisualizer
+//
+//  Created by Vignesh Varadarajan on 5/6/18.
+//  Copyright © 2018 Cyrush LLC. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+}
