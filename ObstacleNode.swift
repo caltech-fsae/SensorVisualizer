587a22bd5c80a0b59bf6d73b6aa8f113ea1a9c43
jagathv <jagathvythee@gmail.com>
2018-06-03 11:58:36 -0700

    added obstacle proto and node

---
+++ ObstacleNode.swift
@@ -0,0 +1,35 @@
+//
+//  SensorNode.swift
+//  SensorVisualizer
+//
+//  Created by Vignesh Varadarajan on 5/6/18.
+//  Copyright © 2018 Cyrush LLC. All rights reserved.
+//
+
+import Cocoa
+import SceneKit
+
+class ObstacleNode: SCNNode {
+    var obstacle: Obstacle?
+
+    init(obstacle: Obstacle) {
+        super.init()
+
+        self.obstacle = obstacle
+
+        self.position = SCNVector3(obstacle.translation.x,
+                                   obstacle.translation.y,
+                                   obstacle.translation.z)
+
+
+
+        let material = SCNMaterial()
+        material.diffuse.contents = NSColor.systemRed
+        self.geometry = init(topRadius: 0.0, bottomRadius: obstacle.radius, height: obstacle.height)
+        self.geometry!.materials = [material]
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+}
